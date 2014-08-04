565964a6b37fc93e112e717c2a4bbb64f397b691
extra <extra@warmmilk.org>
2014-08-03 19:13:06 -0700

    added order list, info and cancel methods

--- bitvc.py
+++ bitvc.py
@@ -3,6 +3,7 @@ import ConfigParser
 import requests
 import md5
 import time
+import pprint
 from errors import error_text
 
 CFG = ConfigParser.ConfigParser()
@@ -20,6 +21,15 @@ def config_map(section):
         return data
     except ConfigParser.NoSectionError:
         return None
+
+def format_check(output):
+    """check for errors and print"""
+    try:
+        msg = error_text(output['code'])
+        print "Error {}: {}".format(output['code'], msg)
+    except KeyError:
+        ppt = pprint.PrettyPrinter(indent=4)
+        ppt.pprint(output)
 
 class BitVC(object):
     """make requests, return data, and stuff"""
@@ -50,10 +60,54 @@ class BitVC(object):
         """
         get personal assets info
 
-        returns: json dict of balances
+        returns:    dict of balances
         """
         sign = self.sign({})
-        params = {'access_key': self.cfg['key'], 'created': sign[1],
-                'sign': sign[0]}
-        req = requests.post(self.cfg['base']+'accountInfo/get', data=params)
-        return req.json()
+        params = {'access_key': self.cfg['key'], 'created': sign[1], 'sign': sign[0]}
+        req = requests.post(self.cfg['base']+'accountInfo/get', data=params).json()
+
+        return req
+
+    def list_orders(self, currency):
+        """
+        get list of orders
+
+        currency:   int 2 = Wright (fiat), 1 = BTC
+
+        returns:    list of order dicts
+        """
+        sign = self.sign({'coin_type': currency})
+        params = {'access_key': self.cfg['key'], 'coin_type': currency, 'created': sign[1], 'sign': sign[0]}
+        req = requests.post(self.cfg['base']+'order/list', data=params).json()
+
+        return req
+
+    def order_info(self, currency, order_id):
+        """
+        get info on a specific order
+
+        currency:   int 2 = Wright (fiat), 1 = BTC
+        order_id:   int order id
+
+        returns:    dict with order info
+        """
+        sign = self.sign({'coin_type': currency, 'id': order_id})
+        params = {'access_key': self.cfg['key'], 'coin_type': currency, 'created': sign[1], 'sign': sign[0]}
+        req = requests.post(self.cfg['base']+'order/'+str(order_id), data=params).json()
+
+        return req
+
+    def order_cancel(self, currency, order_id):
+        """
+        cancel order
+
+        currency:   int 2 = Wright (fiat), 1 = BTC
+        order_id:   int order id
+
+        returns:    dict, check 'Result' key
+        """
+        sign = self.sign({'coin_type': currency, 'id': order_id})
+        params = {'access_key': self.cfg['key'], 'coin_type': currency, 'created': sign[1], 'sign': sign[0]}
+        req = requests.post(self.cfg['base']+'cancel/'+str(order_id), data=params).json()
+
+        return req
